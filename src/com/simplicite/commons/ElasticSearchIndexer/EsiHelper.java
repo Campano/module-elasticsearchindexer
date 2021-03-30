6ffc93f86d3b7c460dd8381a76b13d7579f47203
designer (Designer) <no-reply@simplicite.fr>
2021-03-30 22:49:27 +0200

    init

---
+++ src/com/simplicite/commons/ElasticSearchIndexer/EsiHelper.java
@@ -0,0 +1,24 @@
+package com.simplicite.commons.ElasticSearchIndexer;
+
+import java.util.*;
+import com.simplicite.util.*;
+import com.simplicite.util.tools.*;
+
+/**
+ * Shared code EsiHelper
+ */
+public class EsiHelper implements java.io.Serializable {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 1) Basic strategy
+	 * ---------------------------
+	 * for all non technical modules
+	 *	list objects
+	 *	for each object
+	 *		for each record
+	 *			call ES api with indexable fields / KEY = Object:id
+	 */
+	 
+	 
+}
