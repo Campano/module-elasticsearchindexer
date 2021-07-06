3fc5f1a9a4dc327f04a1dfe66209bbd4f69f8bee
designer (Designer) <no-reply@simplicite.fr>
2021-07-06 18:24:36 +0200

    basic auth + use restTool

--- src/com/simplicite/commons/ElasticSearchIndexer/EsiHelper.java
+++ src/com/simplicite/commons/ElasticSearchIndexer/EsiHelper.java
@@ -14,11 +14,20 @@ public class EsiHelper implements java.io.Serializable {
 	 
 	 Grant g;
 	 String esInstance;
+	 String esIndex;
+	 String esUser;
+	 String esPassword;
 	 
 	 public EsiHelper(Grant g){
 	 	this.g=g;
 	 	JSONObject p = new JSONObject(g.getParameter("ESI_CONFIG"));
 	 	this.esInstance=p.getString("instance");
+	 	this.esIndex=p.optString("index", "simplicite");
+	 	String esCredentials=p.optString("credentials", null);
+	 	if(esCredentials!=null){
+	 		esUser = esCredentials.split(":")[0];
+	 		esPassword = esCredentials.split(":")[0];
+	 	}
 	 }
 	 
 	 public void indexAllModules(){
@@ -51,13 +60,21 @@ public class EsiHelper implements java.io.Serializable {
 	 private void indexEsDoc(String id, JSONObject doc){
 	 	String url = esInstance+"/simplicite/_doc/"+id;
 	 	AppLog.info("Indexing at "+url+" : "+doc.toString(), Grant.getSystemAdmin());
-		String rslt = Unirest
+	 	try{
+	 		String result = RESTTool.post(doc, "application/json", url, esUser, esPassword);
+	 		//AppLog.info("Result : "+result, g);
+	 	}
+	 	catch(Exception e){
+	 		
+	 	}
+	 	
+		/*String rslt = Unirest
 			.put(url)
 			.header("Content-Type", "application/json")
 			.body(doc)
 			.asString()
 			.getBody();
-		AppLog.info("Result : "+rslt, Grant.getSystemAdmin());
+		AppLog.info("Result : "+rslt, Grant.getSystemAdmin());*/
 	 }
 	 
 	 private String[] getModules(){
